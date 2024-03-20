94e8a55836f6f145a8e2ec4f28ac805918b19384
RockefellerEXE <adamwwujkiewicz@gmail.com>
2024-03-20 18:11:16 +0100

    Dodano widoki cząstkowe(view componenty), strony dynamiczne

---
+++ ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepUKW.DAL;
+
+namespace SklepUKW.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        FilmsContext db;
+
+        public MenuViewComponent(FilmsContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = db.Categories.ToList();
+            return await Task.FromResult((IViewComponentResult)View("_Menu", categories));
+        }
+    }
+}
