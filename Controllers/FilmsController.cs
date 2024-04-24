c9093270d285d564d65b110fe51a3b3fc28bf600
RockefellerEXE <adamwwujkiewicz@gmail.com>
2024-04-24 18:18:56 +0200

    Dodawanie plakatów i edycja filmu

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -8,10 +8,12 @@ namespace SklepUKW.Controllers
     public class FilmsController : Controller
     {
         FilmsContext db;
+        private IWebHostEnvironment hostingEnvironment;
 
-        public FilmsController(FilmsContext db)
+        public FilmsController(FilmsContext db, IWebHostEnvironment hostingEnvironment)
         {
             this.db = db;
+            this.hostingEnvironment = hostingEnvironment;
         }
         public IActionResult FilmsList(string categoryName)
         {
@@ -42,12 +44,36 @@ namespace SklepUKW.Controllers
         [HttpPost]
         public IActionResult AddFilm(AddFilmViewModel model) {
             model.Film.AddDate = DateTime.Now;
-            model.Film.Poster = "cube.jpg";
+
+            var picFolderPath = Path.Combine(hostingEnvironment.WebRootPath, "Obrazki");
+            var uniquePosterName = Guid.NewGuid().ToString() + "_"+model.Poster.FileName;
+            model.Film.Poster = uniquePosterName;
+            var fullPath = Path.Combine(picFolderPath,uniquePosterName);
+            model.Poster.CopyTo(new FileStream(fullPath,FileMode.Create));
 
             db.Films.Add(model.Film);
             db.SaveChanges();
 
             return RedirectToAction("Details",new { filmId = model.Film.FilmId });
+        }
+        [HttpGet]
+        public IActionResult EditFilm( int filmId) {
+            var film = db.Films.Where(f => f.FilmId == filmId).FirstOrDefault();
+            var category = db.Categories.Find(film.CategoryId);
+            return View(film);
+        }
+        [HttpPost]
+        public IActionResult EditFilm(Film model)
+        {
+            var film = db.Films.Where(f=>f.FilmId == model.FilmId).FirstOrDefault();
+            film.Title = model.Title;
+            film.Director = model.Director;
+            film.Desc = model.Desc;
+            film.Price = model.Price;
+            db.Entry(film).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Details", new { filmId = model.FilmId });
         }
     }
 }
--- Models/AddFilmViewModel.cs
+++ Models/AddFilmViewModel.cs
@@ -3,6 +3,8 @@
     public class AddFilmViewModel
     {
         public Film Film { get; set; }
+
         public List<Category> Categories { get; set; }
+        public IFormFile Poster { get; set; }
     }
 }
